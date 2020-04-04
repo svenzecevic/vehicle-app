c50997efc3ad630b8fa2c4c526225fb7032aab41
sven_z <sven.zecevic@gmail.com>
2020-04-04 18:00:57 +0200

    -added key to carItem -added PropTypes to Components

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,41 +1,53 @@
-import React, { Component } from "react"
-import CarItem from "./CarItem"
-import Filter from "./Filter"
-import Sort from "./Sort"
-import "./CarList.css"
-import { observer } from "mobx-react"
-
+import React, { Component } from "react";
+import CarItem from "./CarItem";
+import Filter from "./Filter";
+import Sort from "./Sort";
+import "./CarList.css";
+import { observer } from "mobx-react";
+import PropTypes from "prop-types";
 
 @observer
 class CarList extends React.Component {
-filter(e) {
-    this.props.store.filter = e.target.value
-}
-    render(){
-        const cars  = this.props.store.caritems 
-        const filter = this.props.store.filter
-        const filteredCars = this.props.store.filteredCars
-        const carsList = filteredCars.map (car => {
-            return <CarItem make={car.make} model={car.model} className="car"/>
-        })
-        return(
-            <div>
-                 
-                    
-                <Filter value={filter} onChange={this.filter.bind(this)} className="filter"/>
-                
-                
-                <Sort className="sort"/>
-                
-                
-        <li className="cars-list">
-            {carsList}
-        </li>
-        </div>
-        )
-    }
-}
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+  render() {
+    const cars = this.props.store.caritems;
+    const filter = this.props.store.filter;
+    const filteredCars = this.props.store.filteredCars;
+    const carsList = filteredCars.map((car) => {
+      return (
+        <CarItem
+          key={car.id}
+          make={car.make}
+          model={car.model}
+          className="car"
+        />
+      );
+    });
+    return (
+      <div>
+        <Filter
+          value={filter}
+          onChange={this.filter.bind(this)}
+          className="filter"
+        />
 
+        <Sort className="sort" />
 
+        <li className="cars-list">{carsList}</li>
+      </div>
+    );
+  }
+}
+CarItem.propTypes = {
+  make: PropTypes.string,
+  model: PropTypes.string,
+  id: PropTypes.number,
+};
+Filter.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  onChange: PropTypes.func,
+};
 
 export default CarList;
