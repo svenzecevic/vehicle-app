824caa46f16da2671015011c496341cc3e5ca8ee
sven_z <sven.zecevic@gmail.com>
2020-04-19 00:34:09 +0200

    -Added SortButton Component -Implemented sorting functionality

---
+++ src/components/SortButton/SortButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sortButton = (props) => (
+  <button onClick={props.clicked}>Sort by make</button>
+);
+
+export default sortButton;
--- src/containers/CarList.js
+++ src/containers/CarList.js
@@ -5,9 +5,15 @@ import "./CarList.css";
 import { computed, observable } from "mobx";
 import { observer } from "mobx-react";
 import PropTypes from "prop-types";
+import SortButton from "../components/SortButton/SortButton";
 
 @observer
 class CarList extends React.Component {
+  state = {
+    render: true,
+    sortType: true,
+  };
+
   @observable filterState = [];
 
   @computed get filteredCars() {
@@ -16,19 +22,48 @@ class CarList extends React.Component {
       (car) => !this.filterState || filterMatch.test(car.make)
     );
   }
-  filter(e) {
+  filter = (e) => {
     let index = e.nativeEvent.target.selectedIndex;
     let label = e.nativeEvent.target[index].text;
     this.filterState = label;
-  }
+  };
+
+  onSort = () => {
+    this.setState({
+      sortType: !this.state.sortType,
+      render: !this.state.render,
+    });
+  };
 
   render() {
-    const carsList = this.filteredCars.map((car) => {
-      return <CarItem key={car.id} make={car.make} model={car.model} />;
+    const store = this.props.store.caritems;
+    const filteredCars = this.filteredCars;
+    const sorted = store.slice().sort((a, b) => {
+      const isReversed = this.state.sortType === true ? 1 : -1;
+      return isReversed * a.make.localeCompare(b.make);
     });
+
+    let carsList = filteredCars.map((car) => {
+      return (
+        <CarItem
+          id={car.id}
+          make={car.make}
+          model={car.model}
+          className="car"
+        />
+      );
+    });
+    if (this.state.render) {
+      carsList = carsList;
+    } else {
+      carsList = sorted.map((car) => {
+        return <CarItem id={car.id} make={car.make} model={car.model} />;
+      });
+    }
     return (
       <div>
         <Filter onChange={this.filter.bind(this)} />
+        <SortButton clicked={this.onSort} />
 
         <li>{carsList}</li>
       </div>
