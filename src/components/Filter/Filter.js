4f45a054e9a9f7c52915ed493b1e8994bd395781
sven_z <sven.zecevic@gmail.com>
2020-04-20 16:52:01 +0200

    -added bootstrap

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,18 +1,17 @@
 import React from "react";
+import Aux from "../../hoc/Auxillary";
 
-const Sort = (props) => {
-  return (
-    <div>
-      <label>Choose a make:</label>
-      <select onChange={props.onChange} defaultValue={"default"}>
-        <option disabled value="default">
-          Choose a make...
-        </option>
-        <option>Alfa Romeo</option>
-        <option>BMW</option>
-      </select>
-    </div>
-  );
-};
+const Sort = (props) => (
+  <Aux>
+    <label></label>
+    <select onChange={props.onChange} defaultValue={"default"}>
+      <option disabled value="default">
+        Choose a make...
+      </option>
+      <option>Alfa Romeo</option>
+      <option>BMW</option>
+    </select>
+  </Aux>
+);
 
 export default Sort;
--- src/components/SortButton/SortButton.js
+++ src/components/SortButton/SortButton.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const sortButton = (props) => (
-  <button onClick={props.clicked}>Sort by make</button>
+  <button
+    onClick={props.clicked}
+    className="btn btn-primary"
+    data-toggle="button"
+    aria-pressed="false"
+  >
+    Sort by make
+  </button>
 );
 
 export default sortButton;
