618717c2b128d2ebb32be09e1177bade7e1998e1
sven_z <sven.zecevic@gmail.com>
2020-05-13 20:04:40 +0200

    -added lazy routing for EditPage

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Jumbotron from "../../pages/Jumbotron/Jumbotron";
 import MainPage from "../../pages/MainPage/MainPage";
 import { Route, Switch } from "react-router-dom";
 import classes from "./Layout.module.css";
-import EditPage from "../../pages/EditPage/EditPage";
 import NotFound from "../../pages/NotFound/NotFound";
+
+const Edit = React.lazy(() => import("../../pages/EditPage/EditPage"));
 
 const layout = (props) => {
   return (
@@ -12,7 +13,21 @@ const layout = (props) => {
       <Switch>
         <Route path="/" exact component={Jumbotron} />
         <Route path="/main-page" exact component={MainPage} />
-        <Route path="/edit-page" exact component={EditPage} />
+        <Route
+          path="/edit-page"
+          exact
+          render={() => (
+            <Suspense
+              fallback={
+                <div class="d-flex justify-content-center">
+                  <div class="spinner-border" role="status"></div>
+                </div>
+              }
+            >
+              <Edit />
+            </Suspense>
+          )}
+        />
         <Route component={NotFound} />
       </Switch>
     </div>
